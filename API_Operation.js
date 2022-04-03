994cfd092bcb554fad8a37722efea2edf9594591
hryhij <haroldjaplit@gmail.com>
2022-04-03 23:40:00 +0800

    second commit

--- API_Operation.js
+++ API_Operation.js
@@ -23,5 +23,27 @@ async function getGroups() {
     }
 }
 
-module.exports = {getGroups : getGroups}
+async function getGroups_HYBE() {
+    try {
+        let pool = await sql.connect(config)
+        let vgroups = await pool.request()
+            .query('select * from groups where CompanyID="1"')
+            
+            return  vgroups.recordsets;
+    
+        // // Stored procedure
+        
+        // let result2 = await pool.request()
+        //     .input('input_parameter', sql.Int, value)
+        //     .output('output_parameter', sql.VarChar(50))
+        //     .execute('procedure_name')
+        
+        // console.dir(result2)
+        console.log("server connected")
+    } catch (error) {
+        console.log("ito ang errors7 : " + error)
+    }
+}
+
+module.exports = {getGroups : getGroups, getGroups_HYBE:getGroups_HYBE}
 
